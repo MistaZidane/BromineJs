8906ea83408efa905e5907b471af990eef37ddb2
phantomZidane <ntokungwiazidane@yahoo.com>
2020-02-18 09:46:58 +0100

    created the button widget as a test case

---
+++ core/button.js
@@ -0,0 +1,6 @@
+import widget from widget.js
+
+class Button extends Widget {
+
+}
+let btn = new Button()
--- core/widget.js
+++ core/widget.js
@@ -1,6 +1,9 @@
 // the main widget class every other element will inherit from
  class Widget {
-     constructor(atributes, style, event){
-         
+     constructor(type, atributes, style, event){
+         this.name = type;
+         this.atributes = atributes;
+         this.style = style;
+         this.events = event;
      }
  }
